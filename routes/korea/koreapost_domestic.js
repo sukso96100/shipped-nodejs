b78f03957eb05f77461b1dd132817ac2c8913319
sukso96100 <sukso96100@gmail.com>
2016-01-31 22:28:39 +0900

    working on korea post domestic tracker

--- routes/korea/koreapost_domestic.js
+++ routes/korea/koreapost_domestic.js
@@ -1,7 +1,26 @@
+var jsdom = require('jsdom');
+
 var startTask = function(res, postid){
   console.log("KOREAPOST_DOMESTIC : "+postid);
   var url = "https://service.epost.go.kr/trace.RetrieveDomRigiTraceList.comm?sid1="+postid;
-  res.send('koreapost_domestic');
+  jsdom.env(
+  url,
+  ["http://code.jquery.com/jquery.js"],
+  function (err, window) {
+    var jsondata = JSON.stringify({
+      "postid": postid,
+      "carrier": "우체국 - 국내우편(Korea Post - Domestic Posts)",
+      "sender": window.$("tr:eq(1) > td:eq(1)").text(),
+      "receiver": window.$("tr:eq(1) > td:eq(2)").text()
+    });
+    res.send(jsondata);
+
+  }
+);
+
+
+
+
 }
 
 module.exports = startTask;
