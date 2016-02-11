4cdfe51c0611a4926bd1808e3b7327665264cb1e
sukso96100 <sukso96100@gmail.com>
2016-02-11 12:27:10 +0900

    cleanup

--- routes/ko/loader_ko.js
+++ routes/ko/loader_ko.js
@@ -2,19 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 // Korea Post - Domestic
-var koreapost_domestic = require('./koreapost_domestic');
-router.get('/koreapost_domestic/:postid', function(req, res, next) { koreapost_domestic(res, req.params.postid); });
+var koreapost_d = require('./koreapost_d');
+router.get('/koreapost_d/:postid', function(req, res, next) { koreapost_d(res, req.params.postid); });
 
 // Korea Post - International
-var koreapost_international = require('./koreapost_international');
-router.get('/koreapost_international/:postid', function(req, res, next) { koreapost_international(res, req.params.postid); });
+var koreapost_i = require('./koreapost_i');
+router.get('/koreapost_i/:postid', function(req, res, next) { koreapost_i(res, req.params.postid); });
 
 // Logen
 var logen = require('./logen');
 router.get('/logen/:postid', function(req, res, next) { logen(res, req.params.postid); });
 
 // CJ Korea Express
-var cj_korea_express = require('./cj_korea_express');
-router.get('/cj_korea_express/:postid', function(req, res, next) { cj_korea_express(res, req.params.postid); });
+var korex_d = require('./korex_d');
+router.get('/korex_d/:postid', function(req, res, next) { korex_d(res, req.params.postid); });
 
 module.exports = router;
--- routes/us/loader_us.js
+++ routes/us/loader_us.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 // EXMS Express (APEX)
-var ecms_express = require('./ecms_express');
-router.get('/ecms_express/:postid/:i18n', function(req, res, next) { ecms_express(res, req.params.postid, req.params.i18n); });
+var ecms = require('./ecms');
+router.get('/ecms/:postid/:i18n', function(req, res, next) { ecms(res, req.params.postid, req.params.i18n); });
 
 // UPS
 var ups = require('./ups');
