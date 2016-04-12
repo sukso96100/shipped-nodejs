079a235b92cb77ecc1cb10920b8c46f1e368818c
sukso96100 <sukso96100@gmail.com>
2016-04-12 17:18:49 +0900

    working on korean air cargo tracker

--- routes/aircargo/aircargo.js
+++ routes/aircargo/aircargo.js
@@ -6,4 +6,8 @@ var router = express.Router();
 var asiana = require('./asiana');
 router.get('/asiana/:postid/:i18n', function(req, res, next) { asiana(res, req.params.postid, req.params.i18n); });
 
+//Korean Air Cargo
+var koreanait = request('./koreanair');
+router.get('/koreanair/:postid/:i18n', function(req, res, next) { koreanair(res, req.params.postid, req.params.i18n); });
+
 module.exports = router;
---
+++ routes/aircargo/koreanair.js
@@ -0,0 +1,79 @@
+//Korean Air Cargo
+//http://cargo.koreanair.com/ecus/trc/servlet/TrackingServlet
+//12345678
+var prefix = "180";
+var nav = "div#show_div > table > tbody > tr:eq(3) > td > table > tbody > tr \
+ > td:eq(2) > table > tbody > tr > td:eq(0) > form > table > tbody > tr > td >";
+
+var jsdom = require('jsdom');
+var request = require('request');
+var buffer = require('buffer').Buffer;
+var iconv  = require('iconv').Iconv;
+var assert = require('assert');
+var startTask = function(res, postid, i18n){
+  console.log("Korean Air Cargo : "+postid);
+  var url = "http://cargo.koreanair.com/ecus/trc/servlet/TrackingServlet";
+  console.log(url);
+  request({
+  url: url,
+  method: 'POST',
+  body: 'awb_no='+prefix+postid+'&multiAwbNo='+prefix+postid+'&no_awb1='+prefix+'&no_awb2='+postid+'&version='+i18n,
+  headers: {
+    'Origin': 'http://cargo.koreanair.com/',
+    'Referer': 'http://cargo.koreanair.com/ecus/trc/servlet/TrackingServlet',
+    'Content-Type': 'application/x-www-form-urlencoded'}
+  },
+  function (error, response, body){
+    console.log("Res Received");
+
+    if (!error && response.statusCode == 200) {
+
+      // var data = new buffer(body, 'binary');
+      // var converter = new iconv('euc-kr', 'UTF8');
+      // data = converter.convert(data).toString();
+      // console.log(data);
+        console.log(body);
+        jsdom.env( body, ["http://code.jquery.com/jquery.js"],
+        function (err, window) {
+
+          //Info 1
+          var status = [];
+          window.$(nav+"table:eq(9) > tbody > tr")
+            .each(function(index, element){
+              var condition = window.$( element ).children("td:eq(0)").text();
+
+              //Create status array
+              if(index>=1&&condition!="No Data!"){
+                var item = {
+                  "time" : window.$( element ).children("td:eq(2)").text()
+                  + " " + window.$( element ).children("td:eq(3)").text(),
+                  "location" : window.$( element ).children("td:eq(6)").text()
+                  + " - " + window.$( element ).children("td:eq(1)").text()
+                  + " - " + window.$( element ).children("td:eq(7)").text()
+                };
+                status.push(item);
+              }
+            });
+
+
+            var sender = window.$(nav+"table:eq(5) > tbody > tr:eq(1) > td:eq(3)").text();
+            var receiver = window.$(nav+"table:eq(5) > tbody > tr:eq(1) > td:eq(4)").text();
+
+            var jsondata = JSON.stringify({
+              "postid": postid,
+              "url":url,
+              "carrier": "대한항공 항공화물(Korean Air Cargo)",
+              "sender": sender,
+              "receiver": receiver,
+              "status":status
+            });
+            res.send(jsondata);
+            console.log("KOREANAIR - RESPONSE FOR "+ postid + " SENT");
+        });
+    }else{
+      res.send(error);
+    }
+  });
+}
+
+module.exports = startTask;
